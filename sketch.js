cfadd64e5080cff729742459e877571f178f5973
SAMREEN <samreenkhatoon675@gmail.com>
2020-12-04 14:32:55 +0530

    PHYSICS ENGINE

--- sketch.js
+++ sketch.js
@@ -1,9 +1,40 @@
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+
+
+var engine, world, box;
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400, 400);
+  engine = Engine.create()
+  world = engine.world
+
+
+  var options = {
+    isStatic: true
+  }
+
+
+  box = Bodies.rectangle(50, 50, 30, 30, options)
+  World.add(world, box)
+
+  console.log(box)
+
+
+
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(0);
+
+  Engine.update(engine)
+
+  rectMode(CENTER)
+  rect(box.position.x, box.position.y, 30, 30)
+
+
+
 }
